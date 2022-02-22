0bcb92baf2d16e9552f4a85ba53f73f211d68e91
hack_er <70893070+Er4ik@users.noreply.github.com>
2022-02-22 21:13:44 +0200

    fix/add catch exception to all methods

--- src/doublyLinkedList.js
+++ src/doublyLinkedList.js
@@ -13,206 +13,261 @@ class DoublyLinkedList {
     }
 
     get(index) {
-        if (!this.head) return;
+        try {
+            if (!this.head) return;
 
-        let currentValue = this.head;
+            let currentValue = this.head;
 
-        for (let counter = 0; counter !== index; counter++) {
-            currentValue = currentValue.next;
+            for (let counter = 0; counter !== index; counter++) {
+                currentValue = currentValue.next;
+            }
+
+            return currentValue;
+        } catch (error) {
+            throw new Error(`Error get item from list ----> ${error}`);
         }
-
-        return currentValue;
     }
 
     length() {
-        let currentValue = this.head;
-        let counterSizeList = 0;
+        try {
+            let currentValue = this.head;
+            let counterSizeList = 0;
 
-        while (currentValue) {
-            counterSizeList++;
-            currentValue = currentValue.next;
+            while (currentValue) {
+                counterSizeList++;
+                currentValue = currentValue.next;
+            }
+
+            return counterSizeList;
+        } catch (error) {
+            throw new Error(`Error check length of list ----> ${error}`);
         }
 
-        return counterSizeList;
     }
 
     append(data) {
-        const node = new Node(data, this.tail);
-        let currentValue = this.head;
+        try {
+            const node = new Node(data, this.tail);
+            let currentValue = this.head;
 
-        if (!this.head) {
-            this.head = node;
-            return;
-        }
-
-        while (currentValue.next) {
-            currentValue = currentValue.next;
-        }
-
-        [currentValue.next, this.tail] = [node, node]
-        this.tail.previous = currentValue;
-
-        return;
-    }
-
-    insert(data, index) {
-        if (index === this.length()) {
-            this.append(data);
-
-            return;
-        }
-
-        if (index === 0 && this.length() === 1) {
-            this.tail = this.get(index);
-
-            const node = new Node(data, null, this.tail);
-
-            this.tail.previous = node;
-            this.head = node;
-
-            return;
-        }
-
-        if (index === 0) {
-            const node = new Node(data, null, this.head);
-            [this.head, node.next.previous] = [node, node];
-
-            return;
-        }
-        console.log('test');
-        const currentValue = this.get(index);
-        const node = new Node(data, currentValue.previous, currentValue);
-        [currentValue.previous.next, node.previous, node.next, node.next.previous] = [
-            node,
-            currentValue.previous,
-            currentValue,
-            node,
-        ];
-
-        return;
-    }
-
-    delete(index) {
-        if (!this.head) return;
-
-        const value = this.get(index);
-
-        if (index === 0 && this.length() === 1) {
-            [this.head, this.tail] = [null, null];
-
-            return value;
-        }
-
-        if (index === 0 && this.length() > 1) {
-            [this.head, value.next.previous] = [value.next, null];
-
-            return value;
-        }
-
-        if (index === this.length() - 1) {
-            if(value.previous !== this.head) {
-                [this.tail, value.previous.next] = [value.previous, null];
-            } else {
-                [this.tail, value.previous.next] = [null, this.tail];
-            }
-
-            return value;
-        }
-
-        [value.previous.next, value.next.previous] = [value.next, value.previous];
-
-        return value;
-    }
-
-    deleteAll(element) {
-        if (!this.head) return;
-
-        let currentValue = this.head;
-
-        for (let counter = 0; counter < this.length(); counter++) {
-            if (!this.length()) {
-                this.head = null;
-                this.tail = null;
+            if (!this.head) {
+                this.head = node;
                 return;
             }
 
-            if (currentValue.data === element) {
-                this.delete(counter);
-                currentValue = this.head;
-                counter = -1;
-            } else currentValue = currentValue.next;
-        }
+            while (currentValue.next) {
+                currentValue = currentValue.next;
+            }
 
-        return;
+            [currentValue.next, this.tail] = [node, node];
+            this.tail.previous = currentValue;
+
+            return;   
+        } catch (error) {
+            throw new Error(`Error append item to list ----> ${error}`);
+        }
+    }
+
+    insert(data, index) {
+        try {
+            if (index === this.length()) {
+                this.append(data);
+
+                return;
+            }
+
+            if (index === 0 && this.length() === 1) {
+                this.tail = this.get(index);
+
+                const node = new Node(data, null, this.tail);
+
+                this.tail.previous = node;
+                this.head = node;
+
+                return;
+            }
+
+            if (index === 0) {
+                const node = new Node(data, null, this.head);
+                [this.head, node.next.previous] = [node, node];
+
+                return;
+            }
+
+            const currentValue = this.get(index);
+            const node = new Node(data, currentValue.previous, currentValue);
+            [currentValue.previous.next, node.previous, node.next, node.next.previous] = [
+                node,
+                currentValue.previous,
+                currentValue,
+                node,
+            ];
+
+            return;
+        } catch (error) {
+            throw new Error(`Error insert item to list ----> ${error}`);
+        }
+    }
+
+    delete(index) {
+        try {
+            if (!this.head) return;
+
+            const value = this.get(index);
+
+            if (index === 0 && this.length() === 1) {
+                [this.head, this.tail] = [null, null];
+
+                return value;
+            }
+
+            if (index === 0 && this.length() > 1) {
+                [this.head, value.next.previous] = [value.next, null];
+
+                return value;
+            }
+
+            if (index === this.length() - 1) {
+                if (value.previous !== this.head) {
+                    [this.tail, value.previous.next] = [value.previous, null];
+                } else {
+                    [this.tail, value.previous.next] = [null, this.tail];
+                }
+
+                return value;
+            }
+
+            [value.previous.next, value.next.previous] = [value.next, value.previous];
+
+            return value;   
+        } catch (error) {
+            throw new Error(`Error delete item from list ----> ${error}`);
+        }
+    }
+
+    deleteAll(element) {
+        try {
+            if (!this.head) return;
+
+            let currentValue = this.head;
+
+            for (let counter = 0; counter < this.length(); counter++) {
+                if (!this.length()) {
+                    this.head = null;
+                    this.tail = null;
+                    return;
+                }
+
+                if (currentValue.data === element) {
+                    this.delete(counter);
+                    currentValue = this.head;
+                    counter = -1;
+                } else currentValue = currentValue.next;
+            }
+
+            return;   
+        } catch (error) {
+            throw new Error(`Error delete items like "${element}" ----> ${error}`);
+        }
     }
 
     clear() {
-        this.head = null;
-        this.tail = null;
+        try {
+            this.head = null;
+            this.tail = null;
 
-        return;
+            return;
+        } catch (error) {
+            throw new Error(`Error clear list ----> ${error}`);
+        }
+
     }
 
     clone(linkedList = this) {
-        const newLinkedList = new DoublyLinkedList();
+        try {
+            const newLinkedList = new DoublyLinkedList();
 
-        for (let counter = 0; counter < linkedList.length(); counter++) {
-            newLinkedList.append(linkedList.get(counter).data);
+            for (let counter = 0; counter < linkedList.length(); counter++) {
+                newLinkedList.append(linkedList.get(counter).data);
+            }
+
+            return newLinkedList;   
+        } catch (error) {
+            throw new Error(`Error clone list ----> ${error}`);
         }
-        
-        return newLinkedList;
     }
 
     reverse() {
-        if (!this.head) return;
+        try {
+            if (!this.head) return;
 
-        let currentValue = this.head;
-        while (currentValue) {
-            [currentValue.next, currentValue.previous] = [currentValue.previous, currentValue.next];
-            currentValue = currentValue.previous;
+            let currentValue = this.head;
+            while (currentValue) {
+                [currentValue.next, currentValue.previous] = [currentValue.previous, currentValue.next];
+                currentValue = currentValue.previous;
+            }
+
+            [this.head, this.tail] = [this.tail, this.head];
+
+            return;
+        } catch (error) {
+            throw new Error(`Error reverse list ----> ${error}`);
         }
-
-        [this.head, this.tail] = [this.tail, this.head];
-
-        return;
+ 
     }
 
     findFirst(element) {
-        if (!this.head) return;
+        try {
+            if (!this.head) return;
 
-        let currentValue = this.head;
+            let currentValue = this.head;
 
-        if (!this.head) {
-            return -1;
+            if (!this.head) {
+                return -1;
+            }
+
+            for (let counter = 0; counter < this.length(); counter++) {
+                if (currentValue.data === element) return counter;
+                currentValue = currentValue.next;
+            }
+
+            return -1;   
+        } catch (error) {
+            throw new Error(`Error find first item in list ----> ${error}`);
         }
-
-        for (let counter = 0; counter < this.length(); counter++) {
-            if (currentValue.data === element) return counter;
-            currentValue = currentValue.next;
-        }
-
-        return -1;
     }
 
     findLast(element) {
-        if (!this.head) return;
+        try {
+            if (!this.head) return;
 
-        let currentValue = this.tail;
+            let currentValue = this.tail;
 
-        for (let counter = this.length() - 1; counter > 0; counter--) {
-            if (currentValue.data === element) return counter;
-            currentValue = currentValue.previous;
+            for (let counter = this.length() - 1; counter > 0; counter--) {
+                if (currentValue.data === element) return counter;
+                currentValue = currentValue.previous;
+            }
+
+            return -1;
+        } catch (error) {
+            throw new Error(`Error find item in list ----> ${error}`);
         }
-
-        return -1;
     }
 
     extend(linkedList) {
-        if (!linkedList.head) return;
-        const newLinkedList = this.clone(linkedList);
+        try {
+            if (!linkedList.head) return;
+            const newLinkedList = this.clone(linkedList);
 
-        [this.tail.next, newLinkedList.head.previous, this.tail] = [newLinkedList.head, this.tail, newLinkedList.tail];
-        return;
+            [this.tail.next, newLinkedList.head.previous, this.tail] = [
+                newLinkedList.head,
+                this.tail,
+                newLinkedList.tail,
+            ];
+            return;   
+        } catch (error) {
+            throw new Error(`Error extend list ----> ${error}`);
+        }
     }
 }
 
